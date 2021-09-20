462b4d9b838e334549509a19badf6ab9297a7881
jbmakib <mdfoujolmobin@gmail.com>
2021-09-20 22:25:40 +0600

    input field added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -9,8 +9,32 @@ const Countries = () => {
             .then((res) => res.json())
             .then((data) => setCountries(data));
     }, []);
+
+    // search by input field
+    function changeApi() {
+        const name = document.getElementById("input-field").value;
+        fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+            .then((res) => res.json())
+            .then((data) => setCountries(data));
+    }
     return (
-        <div>
+        <div className="container">
+            <div className="input-group my-3">
+                <input
+                    type="text"
+                    id="input-field"
+                    className="form-control"
+                    placeholder="Search By Product Name"
+                />
+
+                <button
+                    className="btn btn-outline-dark"
+                    id="search-btn"
+                    onClick={changeApi}
+                >
+                    Search
+                </button>
+            </div>
             <h1>Countries Found: {countries.length}</h1>
             <div className="countries">
                 {countries.map((country) => (
